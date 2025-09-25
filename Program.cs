d4d08fbffcc3d65828568138266dd848ed059154
mdreed89 <meri@quavered.com>
2025-09-25 14:03:31 -0500

    Added logic to take user input and provide response.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,25 @@ class Program
 {
   static void Main(string[] args)
   {
-    DuckFlowerApp.Run();
+    int number;
+    while (true)
+    {
+      Console.WriteLine("Please enter a number between 1 and 100:");
+      string userInput = Console.ReadLine();
+
+      if (int.TryParse(userInput, out number) && number >= 1 && number <= 100)
+      {
+        DuckFlowerApp.Run(number);
+        break;
+      }
+      else Console.WriteLine("Invalid input. Please enter a valid number between 1 and 100.");
+    }
   }
 }
+/*
+Add a do-while loop: 
+First time, user is asked to enter a number. 
+After the while loop ends, ask user if they want to enter another number. - Validate reponse
+Repeat loop until user responds 'n'.
+
+*/
--- apps/DuckFlowerApp.cs
+++ apps/DuckFlowerApp.cs
@@ -1,38 +1,15 @@
-/*
-Duck-Flower Coding Challenge
-Objective
-Write a C# program that prints the numbers from 1 to 100.
-However:
-
-For numbers divisible by 3, print "Duck" instead of the number.
-
-For numbers divisible by 5, print "Flower" instead of the number.
-
-For numbers divisible by both 3 and 5, print "DuckFlower" instead of the number.
-
-Requirements
-Your program should loop from 1 to 100 (inclusive).
-
-Each output should be printed on a new line.
-
-The program should correctly substitute words according to the rules above.
-
-The solution should be written in C# and be self-contained (no dependencies other than standard .NET libraries).
- */
 using System;
 namespace MyApplication;
 
 public static class DuckFlowerApp
 {
-    public static void Run()
+    public static void Run(int number)
     {
-        for (int i = 1; i <= 100; i++)
-        {
-            if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("DuckFlower");
-            else if (i % 3 == 0) Console.WriteLine("Duck");
-            else if (i % 5 == 0) Console.WriteLine("Flower");
-            else Console.WriteLine(i);
 
-        }
+        if (number % 3 == 0 && number % 5 == 0) Console.WriteLine("DuckFlower");
+        else if (number % 3 == 0) Console.WriteLine("Duck");
+        else if (number % 5 == 0) Console.WriteLine("Flower");
+        else Console.WriteLine(number);
+
     }
 }
